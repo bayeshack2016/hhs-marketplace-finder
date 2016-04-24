c6f99c5eb770abeb751bb543a4f74004b5055e12
Dillon Niederhut <dillon.niederhut@gmail.com>
2016-04-24 01:48:10 -0700

    adding search functions into views

--- www/app/views.py
+++ www/app/views.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template, request
+from . import search
 from wtforms import Form, IntegerField, SelectField, StringField, ValidationError
 from wtforms.validators import DataRequired, Length, NumberRange, Optional
 
@@ -19,13 +20,16 @@ class InForm(Form):
     doctor = StringField('Your doctor', validators=[Optional()])
 
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = InForm(request.form)
     if (request.method == 'POST') & form.validate():
         params = {}
         params['state'] = 'Arkansas'
+        params['locals'] = search.get_locals(form.zip_code.data)
+        if form.doctor.data:
+            fname, lname = search.split_name(form.doctor.data)
+            params['plans'] = search.get_plans(fname, lname)
         return render_template('index.html', form=form, **params)
     else:
         print(request.method)
