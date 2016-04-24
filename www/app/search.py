2539720bf36ba4f963d56759f2a2748935203020
Dillon Niederhut <dillon.niederhut@gmail.com>
2016-04-23 22:07:47 -0700

    initial commit of query helpers

---
+++ www/app/search.py
@@ -0,0 +1,14 @@
+#!/bin/env python
+
+import re
+
+def split_name(name_string):
+    name_string = name_string.strip()
+    name_string = name_string.sub(r'\w+\.', '', name_string)
+    fname = name_string.split()[0]
+    lname = name_string.split()[-1] #not 1 because we want to skip middle name
+    return fname, lname
+
+def get_plans(fname, lname):
+
+def get_locals(plan):
