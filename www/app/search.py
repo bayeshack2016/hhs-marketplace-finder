b7fb9dea1f7604e1caf43bf4bf610ab49156d92f
Dillon Niederhut <dillon.niederhut@gmail.com>
2016-04-23 23:52:02 -0700

    name error

--- www/app/search.py
+++ www/app/search.py
@@ -4,7 +4,7 @@ import re
 
 def split_name(name_string):
     name_string = name_string.strip()
-    name_string = name_string.sub(r'\w+\.', '', name_string)
+    name_string = re.sub(r'\w+\.', '', name_string)
     fname = name_string.split()[0]
     lname = name_string.split()[-1] #not 1 because we want to skip middle name
     return fname, lname
