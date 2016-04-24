38f81073130a088f58d55e1442138268210957bb
Dillon Niederhut <dillon.niederhut@gmail.com>
2016-04-24 02:08:14 -0700

    adding new form inputs

--- www/app/views.py
+++ www/app/views.py
@@ -14,10 +14,25 @@ class InForm(Form):
         except:
             raise ValidationError("Not a valid zip code")
 
-    age = IntegerField('Your age', validators=[Optional()])
-    sex = SelectField('Your gender', choices=[('decline', 'decline to share'), ('female', 'female'), ('male', 'male'), ('trans', 'transgendered')], validators=[Optional()])
-    smoking = SelectField('Your smoking', choices=[('decline', 'decline to share'), ('never did', 'never did'), ('used to', 'used to'), ('still do', 'still do')], validators=[Optional()])
-    doctor = StringField('Your doctor', validators=[Optional()])
+    age = IntegerField('How old are you?', validators=[Optional()])
+
+    sex = SelectField('How do you identify?', choices=[('decline', 'decline to share'), ('female', 'female'), ('male', 'male'), ('trans', 'transgendered')], validators=[Optional()])
+
+    smoking = SelectField('Have you smoked?', choices=[('decline', 'decline to share'), ('never did', 'never did'), ('used to', 'used to'), ('still do', 'still do')], validators=[Optional()])
+
+    physical = SelectField('Do you have a physical condition that affects your everyday quality of life?', choices=[('decline', 'decline to share'), ('no', 'no'), ('yes', 'yes')], validators=[Optional()])
+
+    mental = SelectField('Do you have a mental condition that affects your everyday quality of life?', choices=[('decline', 'decline to share'), ('no', 'no'), ('yes', 'yes')], validators=[Optional()])
+
+    pregnant = SelectField('Are you pregnant, or planning on becoming pregnant?', choices=[('decline', 'decline to share'), ('no', 'no'), ('yes', 'yes')], validators=[Optional()])
+
+    hospital = IntegerField('How many nights did you spend in a hospital last year?', validators=[Optional()])
+
+    education = SelectField('What is the highest educational level you have obtained?', choices=[('decline', 'decline to share'), ('no diploma', 'no diploma'), ('high school or GED', 'high school or GED'), ('college', 'college'), ('postgraduate', 'postgraduate')], validators=[Optional()])
+
+    income = SelectField('How much money did you make last year?', choices=[('decline', 'decline to share'), ('< $20,000', '< $20,000'), ('$20,000 < $45,000', '$20,000 < $45,000'), ('$45,000 < $75,000', '$45,000 < $75,000'), ('> $75,000', '> $75,000')], validators=[Optional()])
+    
+    doctor = StringField('If you have a doctor you like, enter their name here', validators=[Optional()])
 
 
 @app.route('/', methods=['GET', 'POST'])
